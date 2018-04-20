002d501953d106296bc266679124e0bcaea992cb
tfabiha <tfabiha@stuy.edu>
2018-04-19 20:14:47 -0400

    completed core methods

--- Deque.java
+++ Deque.java
@@ -1,12 +1,13 @@
 public interface Deque<E>
 {
     void addFirst(E val);
-    //void addLast(E val);
+    void addLast(E val);
 
-    //E pollFirst();
-    //E pollLast();
+    E pollFirst();
+    E pollLast();
 
-    //E peekFirst();
-    //E peekLast();
-	
+    E peekFirst();
+    E peekLast();
+
+    
 }
--- QQKachoo.java
+++ QQKachoo.java
@@ -13,9 +13,90 @@ public class QQKachoo<E> implements Deque<E>
 
     public void addFirst(E val)
     {
-	DLLNode<E> newVal = new DLLNode<E>(val, null _first);
-	_first.setPrev(newVal);
-	_first = newVal;
+	DLLNode<E> newVal = new DLLNode<E>(val, null, null);
+
+	if (_size == 0)
+	    {
+		_first = newVal;
+		_last = newVal;
+	    }
+	else
+	    {
+		newVal.setNext(_first);
+	        _first.setPrev(newVal);
+
+		_first = newVal;
+	    }
+
+	_size += 1;
+    }
+
+    public void addLast(E val)
+    {
+	DLLNode<E> newVal = new DLLNode<E>(val, null, null);
+
+	if (_size == 0)
+	    {
+		_first = newVal;
+		_last = newVal;
+	    }
+	else
+	    {
+		newVal.setPrev(_last);
+	        _last.setNext(newVal);
+
+		_last = newVal;
+	    }
+
+	_size += 1;
+    }
+
+    public E pollFirst()
+    {
+	if (_size == 0)
+	    return null;
+
+	E temp = _first.getCargo();
+
+	_first = _first.getNext();
+
+	if (_size > 1)
+	    _first.setPrev(null);
+	
+	_size -= 1;
+	return temp;
+    }
+
+    public E pollLast()
+    {
+	if (_size == 0)
+	    return null;
+
+	E temp = _last.getCargo();
+
+	_last = _last.getPrev();
+
+	if (_size > 1)
+	    _last.setNext(null);
+	
+	_size -= 1;
+	return temp;
+    }
+
+    public E peekFirst()
+    {
+	if (_size == 0)
+	    return null;
+
+	return _first.getCargo();
+    }
+    
+    public E peekLast()
+    {
+	if (_size == 0)
+	    return null;
+
+	return _last.getCargo();
     }
 
     public String toString()
@@ -23,16 +104,16 @@ public class QQKachoo<E> implements Deque<E>
 	String ret = "";
 	DLLNode<E> temp = _first;
 	
-	for (int x = 0; x < _size - 1; x++)
+	for (int x = 0; x < _size; x++)
 	    {
-		ret += temp.toString();
+		ret += temp.toString() + " ";
 		temp = temp.getNext();
 	    }
 	return ret;
     }
-
     
     public static void main(String[] args)
-    {}
+    {
+    }
 }
 				   
